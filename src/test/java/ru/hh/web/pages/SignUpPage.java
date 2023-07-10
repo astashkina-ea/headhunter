3066b0f1039174077d18d681cbc8f6c90ed34766
Ekaterina Astaskina <ekat9615@gmail.com>
2023-07-10 22:03:05 +0300

    added fix

--- src/test/java/ru/hh/web/pages/SignUpPage.java
+++ src/test/java/ru/hh/web/pages/SignUpPage.java
@@ -9,6 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class SignUpPage {
 
     private final SelenideElement signupWrapper = $(".signup-wrapper");
+
     @Step("Проверить открытие страницы регистрации")
     public SignUpPage checkOpeningSignupPage() {
         signupWrapper.shouldBe(appear );
